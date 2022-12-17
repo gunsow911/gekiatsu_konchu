95833afb99290cffb809c25628257e2309c0ef35
gunsow <fukudatatsuya911@gmail.com>
2022-12-17 14:14:50 +0900

    地図を表示させた

---
+++ components/Map.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {MapContainer, TileLayer} from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const Map = () => {
+  return (
+  <>
+    <MapContainer center={[34.1046934,131.3046877]} zoom={13} style={{width: '100%', height: '100vh'}}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+    </MapContainer>
+  </>
+  )
+}
+
+export default Map
+
