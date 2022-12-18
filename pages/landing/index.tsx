f06153c769fc9fc3e8aa49cb8b06b367c9aa95e6
greenthinktank <radionomoney@gmail.com>
2022-12-18 14:14:09 +0900

    landingページを作成、画像も追加

---
+++ pages/landing/index.tsx
@@ -0,0 +1,38 @@
+import Head from 'next/head'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+// import styles from '../styles/Landing.module.css'
+
+export default function Landing() {
+  return (
+    <div>
+      <Head>
+        <title>激アツ！昆虫マップ ランディング</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main style={{
+          textAlign: 'center',
+          margin: '50px 100px'
+          }}>
+        <div style={{
+          textAlign: 'center',
+          margin: '50px 100px',
+          }}>
+          <Image className='logo_en' src="/logo/logo_ja.svg" alt="Logo" width={350} height={160} />
+        </div>
+        <div style={{marginBottom: '3em'}}>
+          <div style={{marginBottom: '1.2em'}}>
+            <h1>激アツ！昆虫マップ とは？</h1>
+          </div>
+          <p>
+            身近な自然の中に潜む昆虫の種別をマップ上で可視化できるツールです。<br/>
+            昆虫の好きな樹種からそれを好む昆虫のいそうな場所をマッピングします。
+            このマップを見ることにより身近な森林が昆虫ワクワクゾーンに大変身。
+          </p>
+        </div>
+        <img style={{marginBottom: '1.2em', width: '100px'}} src="/landing/topimg.png" alt="" />
+      </main>
+    </div>
+  )
+}
