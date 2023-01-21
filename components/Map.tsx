b7ac76564252108e1b44a60adec5bb0166fad6e3
gunsow <fukudatatsuya911@gmail.com>
2023-01-21 17:42:42 +0900

    スマホ対応のために地図の高さをvhからdvhに変更した

--- components/Map.tsx
+++ components/Map.tsx
@@ -27,7 +27,7 @@ const Map = () => {
   }, [initLayer])
 
   return (
-    <MapContainer center={initCenter ?? {lat: 34.18583, lng: 131.47139}} zoom={13} style={{width: '100%', height: '100vh'}}>
+    <MapContainer center={initCenter ?? {lat: 34.18583, lng: 131.47139}} zoom={13} style={{width: '100%', height: '100dvh'}}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
