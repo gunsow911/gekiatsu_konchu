fa6cc8d4e0fdbbbb3b5eb971bcaadbab673efd98
gunsow <fukudatatsuya911@gmail.com>
2023-01-21 15:53:05 +0900

    viewportの設定をした

--- components/controls/LinkLandingPageControl.tsx
+++ components/controls/LinkLandingPageControl.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 
 const LinkLandingPageControl = () => {
   return (
-    <div className="leaflet-top leaflet-right mb-4">
+    <div className="leaflet-top leaflet-right">
       <div className="leaflet-control leaflet-bar px-1 bg-white text-gray-900">
         <Link className="leaflet-landing-link" href="/landing" passHref>ページに戻る</Link>
       </div>
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
       <Head>
         <title>激アツ！昆虫マップ</title>
         <link rel="icon" href="/favicon.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <body className="font-body">
         <main>
